cee8d6fadb20e635c071b62d3124b854dcbd1762
Robert Wagner <rwwagner@vt.edu>
2016-06-29 12:50:23 -0400

    Update CLI, fix deprecations

--- addon/initializers/meta.js
+++ addon/initializers/meta.js
@@ -1,14 +1,7 @@
 export default {
   name: 'meta',
 
-  initialize(app, oldApp) {
-    // Ember 1.3 compat
-    if(oldApp.inject) {
-      oldApp.inject('route', 'meta', 'service:meta');
-    }
-    // Ember 2.x
-    else {
-      app.inject('route', 'meta', 'service:meta');
-    }
+  initialize(app) {
+    app.inject('route', 'meta', 'service:meta');
   }
 };
--- addon/instance-initializers/meta.js
+++ addon/instance-initializers/meta.js
@@ -1,24 +1,14 @@
 export default {
   name: 'meta',
 
-  initialize(applicationInstance) {
-    const meta = lookup(applicationInstance, 'service:meta');
+  initialize(appInstance) {
+    const meta = appInstance.lookup('service:meta');
 
     meta.storeDefaultTitle();
 
     // Bind on didTransition
-    lookup(applicationInstance, 'router:main').on('didTransition', () => {
+    appInstance.lookup('router:main').on('didTransition', () => {
       meta.insert();
     });
   }
 };
-
-function lookup(applicationInstance, name) {
-  // Ember 2.x
-  if (applicationInstance.lookup) {
-    return applicationInstance.lookup(name);
-  }
-
-  // Ember 1.13 compat
-  return applicationInstance.container.lookup(name);
-}
