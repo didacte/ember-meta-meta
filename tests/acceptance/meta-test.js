9b495d6365200f8bfcee1bd8ff201d68c9151464
Léo Renaud-Allaire <leo.renaud@hooktstudios.com>
2019-03-27 14:45:12 -0400

    Run ember codemods

--- tests/acceptance/meta-test.js
+++ tests/acceptance/meta-test.js
@@ -1,58 +1,56 @@
-import {test} from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
-import {title, description, metaName, metaProperty} from '../helpers/document-meta';
+import { visit } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import {
+  title,
+  description,
+  metaName,
+  metaProperty
+} from '../helpers/document-meta';
 
-moduleForAcceptance('Acceptance | meta');
+module('Acceptance | meta', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('Default values', (assert) => {
-  assert.expect(3);
+  test('Default values', async assert => {
+    assert.expect(3);
 
-  visit('/');
+    await visit('/');
 
-  andThen(() => {
     assert.ok(title().match(new RegExp("^My Custom Default Title - My Website")));
     assert.equal(description(), 'This Is Default Dummy Description');
     assert.equal(metaProperty('og:image'), 'https://exemple.net/my-logo.png');
   });
-});
 
 
-test('Visit all pages', (assert) => {
-  assert.expect(16);
+  test('Visit all pages', async assert => {
+    assert.expect(16);
 
-  visit('/news');
+    await visit('/news');
 
-  andThen(() => {
     assert.ok(title().match(new RegExp("^This Is News Title - My Website")));
     assert.equal(description(), 'This Is News Description');
     assert.equal(metaProperty('og:image'), 'https://exemple.net/latest-news.png');
     // Meta not existing in initial DOM
     assert.equal(metaName('twitter:author'), '@j15e');
     assert.equal(metaProperty('og:description'), 'News Description for Open Graph');
-  });
 
-  visit('/about');
+    await visit('/about');
 
-  andThen(() => {
     assert.ok(title().match(new RegExp("^This Is About Title - My Website")));
     assert.equal(description(), null);
     assert.equal(metaProperty('og:image'), null);
     // Meta not existing in initial DOM
     assert.equal(metaName('twitter:author'), null);
     assert.equal(metaProperty('og:description'), null);
-  });
 
-  visit('/empty');
+    await visit('/empty');
 
-  andThen(() => {
     assert.ok(title().match(new RegExp("^My Custom Default Title - My Website")));
     assert.equal(description(), null);
     assert.equal(metaProperty('og:image'), null);
-  });
 
-  visit('/');
+    await visit('/');
 
-  andThen(() => {
     assert.ok(title().match(new RegExp("^My Custom Default Title - My Website")));
     assert.equal(description(), null);
     assert.equal(metaProperty('og:image'), null);
